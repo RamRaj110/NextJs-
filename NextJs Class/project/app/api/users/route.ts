df0703f0fe0dc6b64930d3d78b0a1e5e93d5c967
RamRaj110 <rampawankumar110@gmail.com>
2025-12-23 18:13:48 +0530

    create api

--- NextJs Class/project/app/api/users/route.ts
+++ NextJs Class/project/app/api/users/route.ts
@@ -1,6 +1,8 @@
 import User from "@/database/user.modules";
 import handleError from "@/lib/handlers/errors";
+import { ValidationError } from "@/lib/http-errors";
 import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validation";
 import { APIErrorResponse } from "@/Types/global";
 import { NextResponse } from "next/server";
 
@@ -14,4 +16,17 @@ export async function GET() {
         return handleError(error,"api") as APIErrorResponse
     }
 }
-export async function POST(request:Request) 
+export async function POST(request:Request){
+    try {
+        await dbConnect();
+        const body = await request.json();
+        const validatedData  = UserSchema.safeParse(body);
+        if(!validatedData.success){
+        throw new ValidationError(validatedData.error.flatten().fieldErrors);
+        }
+        const {email,username} = validatedData.data;
+        const existingUser = await User.findOne({$or:[{email},{username}]});
+    } catch (error) {
+        
+    }
+}
