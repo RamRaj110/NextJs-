145489e59eaec647bd38027210b3e8c1658d184f
RamRaj110 <rampawankumar110@gmail.com>
2026-01-12 13:13:59 +0530

    bug fix

--- NextJs Class/project/lib/handlers/action.ts
+++ NextJs Class/project/lib/handlers/action.ts
@@ -1,4 +1,4 @@
-'use server'
+"use server";
 import { ZodError } from "zod";
 import { z } from "zod";
 import { UnauthorizedError, ValidationError } from "../http-errors";
@@ -6,41 +6,41 @@ import { Session } from "next-auth";
 import dbConnect from "../mongoose";
 import { auth } from "@/auth";
 
-type ActionOptions<T>={
-    params?:T;
-    schema?:z.ZodType<T>;
-    authorize?:boolean;
-}
+type ActionOptions<T> = {
+  params?: T;
+  schema?: z.ZodType<T>;
+  authorize?: boolean;
+};
 
 async function action<T>({
-    params,
-    schema,
-    authorize = false,
-}:ActionOptions<T>){
-    if(schema && params){
-        try {
-            const validatedParams = schema.parse(params);
-            params = validatedParams;
-        } catch (error) {
-            if(error instanceof ZodError){
-                return new ValidationError(
-                    error.flatten().fieldErrors as Record<string,string[]>
-                )
-            }else{
-                return new Error("Schema validation failed");
-            }
-        }
+  params,
+  schema,
+  authorize = false,
+}: ActionOptions<T>) {
+  if (schema && params) {
+    try {
+      const validatedParams = schema.parse(params);
+      params = validatedParams;
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return new ValidationError(
+          error.flatten().fieldErrors as Record<string, string[]>
+        );
+      } else {
+        return new Error("Schema validation failed");
+      }
     }
+  }
 
-    let session : Session | null = null;
-    if(authorize){
-        session = await auth()
-      if(!session){
-        throw new UnauthorizedError()
+  let session: Session | null = null;
+  if (authorize) {
+    session = await auth();
+    if (!session) {
+      return new UnauthorizedError();
     }
-}
-    await dbConnect();
-    return {params, session};
+  }
+  await dbConnect();
+  return { params, session };
 }
 
 export default action;
